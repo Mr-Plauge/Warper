117d9487686e54d6f9cf2aafe8adec4ffc0db5b1
Mr-Plague <65480709+Mr-Plauge@users.noreply.github.com>
2022-04-21 10:51:12 -0700

    small fix

--- src/main/java/io/github/mrplague/mixin/BackgroundRendererMixin.java
+++ src/main/java/io/github/mrplague/mixin/BackgroundRendererMixin.java
@@ -12,39 +12,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static io.github.mrplague.MrPlagueWarperClient.MC;
+
 @Mixin(BackgroundRenderer.class)
 public class BackgroundRendererMixin {
-    @Shadow
-    private static float red;
-
-    @Shadow
-    private static float blue;
-
-    @Shadow
-    private static float green;
-
-
-    @Inject(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/BackgroundRenderer;blue:F", opcode = Opcodes.PUTSTATIC, ordinal = 0, shift = At.Shift.AFTER), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/util/CubicSampler;sampleColor(Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/CubicSampler$RgbFetcher;)Lnet/minecraft/util/math/Vec3d;")))
-    private static void setFogColorToSkyColor(Camera camera, float partialTicks, ClientWorld world, int i, float f, CallbackInfo info) {
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private static void render(Camera camera, float tickDelta, ClientWorld world, int i, float f, CallbackInfo ci) {
         if (MrPlagueWarperClient.enabled) {
-            if (MrPlagueWarperClient.viewed_dim == 1)
-            {
-                BackgroundRendererMixin.red = 0f;
-                BackgroundRendererMixin.green = 0.05f;
-                BackgroundRendererMixin.blue = 0.01f;
-            }
-            else if (MrPlagueWarperClient.viewed_dim == 2)
-            {
-                BackgroundRendererMixin.red = 0.05f;
-                BackgroundRendererMixin.green = 0f;
-                BackgroundRendererMixin.blue = 0.01f;
-            }
-            else
-            {
-                BackgroundRendererMixin.red = 0.03f;
-                BackgroundRendererMixin.green = 0f;
-                BackgroundRendererMixin.blue = 0.03f;
-            }
+            ci.cancel();
         }
     }
 }
